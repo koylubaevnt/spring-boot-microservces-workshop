7145c7c7090984cd8f0b2486192ac43bdfea52ef
Nikita Koylubaev <NKoylubaev@ibs.ru>
2020-03-31 00:31:15 +0500

    Hard coded ratings in catalog service & Used RestTemplate to call Movie Info API

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,13 +1,17 @@
 package io.javabrains.moviecatalogservice.resources;
 
 import io.javabrains.moviecatalogservice.model.CatalogItem;
+import io.javabrains.moviecatalogservice.model.Movie;
+import io.javabrains.moviecatalogservice.model.Rating;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
@@ -15,8 +19,25 @@ public class MovieCatalogResource {
 
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        return Collections.singletonList(
-                new CatalogItem("Transformers", "Test", 4)
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        // get all rated movie IDs
+        List<Rating> ratings = Arrays.asList(
+            new Rating("1234", 4),
+            new Rating("5678", 3)
         );
+
+        return ratings.stream()
+                .map(rating -> {
+                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                    return new CatalogItem(movie.getName(), "Desc", rating.getRating());
+                })
+                .collect(Collectors.toList());
+
+        // for each movie ID call movie info service and get details
+
+        // put them together
+
     }
 }
