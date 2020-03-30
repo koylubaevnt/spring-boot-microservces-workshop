c1eeeab53ff9c196ad13cff0d579195eb388ad3d
Nikita Koylubaev <NKoylubaev@ibs.ru>
2020-03-30 23:40:51 +0500

    Added Movie Catalog API

---
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/model/CatalogItem.java
@@ -0,0 +1,15 @@
+package io.javabrains.moviecatalogservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CatalogItem {
+
+    private String name;
+    private String description;
+    private int rating;
+}
