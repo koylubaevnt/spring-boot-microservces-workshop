a64b499ea1058f13e4e09def00a292359aa51d17
Nikita Koylubaev <NKoylubaev@ibs.ru>
2020-04-05 22:03:09 +0500

    Add Spring Cloud Config Service

---
+++ spring-cloud-config-service/src/test/java/io/javabrains/springcloudconfigservice/SpringCloudConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.springcloudconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
